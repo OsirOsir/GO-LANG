03819a3e92b1de5a1d559f76b0319e93103cbce5
Philip Osir <philiphenril@gmail.com>
2024-03-23 15:24:54 +0300

    functions.go

---
+++ BOOT.DEV/functions.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func concat(s1 string, s2 string) string {
+	return s1 + s2
+}
+
+func main() {
+	test("Lane,", " happy birthday!")
+	test("Elon,", " hope that Tesla thing works out")
+	test("Go", " is fantastic")
+}
+
+// don't touch below this line
+func test(s1 string, s2 string) {
+	fmt.Println(concat(s1, s2))
+}
