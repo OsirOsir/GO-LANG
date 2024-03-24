103d9d8a9fc683bcf50ede8fe1ed454c0811abbb
Philip Osir <philiphenril@gmail.com>
2024-03-24 13:42:57 +0300

    example5.go

---
+++ POINTERS/example5.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	b := 255
+
+	a := &b
+
+	fmt.Println("address of b is ", a)
+	fmt.Println("value of b is ", *a)
+	*a++
+	fmt.Println("new value of b is", b)
+}
