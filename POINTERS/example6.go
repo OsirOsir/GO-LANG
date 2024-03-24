ac0b8c3bc1f7b7e6b7dc6718ffd73e4cd8b94066
Philip Osir <philiphenril@gmail.com>
2024-03-24 14:08:34 +0300

    all

---
+++ POINTERS/example6.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func change(val *int) {
+	*val = 55
+}
+
+func main() {
+	a := 58
+	fmt.Println("value of a before function call is", a)
+	b := &a
+
+	change(b)
+	fmt.Println("value after function call is", a)
+}
