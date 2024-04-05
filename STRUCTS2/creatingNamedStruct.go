59251e6b38a71d6397a0638372f4c358a89b2709
Philip Osir <philiphenril@gmail.com>
2024-04-05 09:49:06 +0300

    creatingNamedStruct.go

---
+++ STRUCTS2/creatingNamedStruct.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type employee struct {
+	firstName string // firstName is a field
+	lastName  string // lastName is also a field
+	age       int
+	salary    int
+}
+
+func main() {
+	emp1 := employee{
+		firstName: "Joshua",
+		lastName:  "Otieno",
+		age:       35,
+		salary:    30000,
+	}
+	fmt.Println(emp1)
+
+	//  we can also creat structs without specifying the fields
+
+	emp2 := employee{"Philip", "Osir", 26, 30000}
+
+	fmt.Println(emp2)
+}
