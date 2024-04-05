3d51d377adf51162392ac34ffe382ddd2db66b99
Philip Osir <philiphenril@gmail.com>
2024-04-05 16:48:53 +0300

    updates

--- STRUCTS2/bill.go
+++ STRUCTS2/bill.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type bill struct {
 	name  string
 	items map[string]float64
@@ -9,9 +11,26 @@ type bill struct {
 func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		items: map[string]float64{},
+		items: map[string]float64{"pie": 5.99, "cake": 3.99},
 		tip:   0,
 	}
 
 	return b
+}
+
+func (b bill) format() string {
+	fs := "Bill breakdown: \n"
+
+	var total float64 = 0
+
+	//list items
+	for k, v := range b.items {
+		fs = fs + fmt.Sprintf("%-25v   ...$%v \n", k+":", v)
+		total = total + v
+	}
+
+	//total
+	fs = fs + fmt.Sprintf("%-25v   ...$%0.2f", "total:", total)
+
+	return fs
 }
--- STRUCTS2/sprintFlearn.go
+++ STRUCTS2/sprintFlearn.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	name := "Alice"
+	name := "Alice\n"
 	age := 7
 
 	name = fmt.Sprintf("Hello my name is %s  and I am %d years old", name, age)
