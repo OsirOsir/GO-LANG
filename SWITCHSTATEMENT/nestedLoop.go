3df13e1b2b1ed9592d04ac56fabbbebda77eeb4b
Philip Osir <philiphenril@gmail.com>
2024-04-11 17:36:05 +0300

    updates

---
+++ SWITCHSTATEMENT/nestedLoop.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// n := 5
+
+	// for i := 0; i < n; i++ {
+	// 	for j := 0; j <= i; j++ {
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	for i := 0; i < 3; i++ {
+		for j := 1; j < 4; j++ {
+			fmt.Printf("i = %d , j = %d\n", i, j)
+		}
+	}
+}
