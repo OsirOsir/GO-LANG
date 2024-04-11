67e7cf817310b70f640d64384e5ff535464a6d5f
Philip Osir <philiphenril@gmail.com>
2024-04-11 23:09:22 +0300

    updates

--- SWITCHSTATEMENT/loopExample.go
+++ SWITCHSTATEMENT/loopExample.go
@@ -3,13 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	x := 0
 
-	for x < 1000 {
+	for x := 0; x <= 1000; x++ {
 		if x != 0 && x%3 == 0 && x%7 == 0 && x%9 == 0 {
 			fmt.Println(x)
-			x++
-			break
+			continue
 		}
 	}
 }
