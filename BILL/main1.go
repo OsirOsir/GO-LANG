b2e1249c8e4472dacc64d9065973276deeebe406
Philip Osir <philiphenril@gmail.com>
2024-04-15 10:09:53 +0300

    updates

--- BILL/main1.go
+++ BILL/main1.go
@@ -34,7 +34,29 @@ func promptOptions(i invoice) {
 
 	opt, _ := getInput("Chose from a - addItem, t - addTip, s - SaveInvoice ", reader)
 
-	fmt.Println("You Chose :", opt)
+	// fmt.Println("You Chose :", opt)
+
+	switch opt {
+	case "a":
+		item, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item Price: ", reader)
+
+		fmt.Println("Bill is :", item, price)
+		promptOptions(i)
+
+	case "t":
+		tip, _ := getInput("Add tip: ($)", reader)
+
+		fmt.Println("$", tip)
+		promptOptions(i)
+
+	case "s":
+		fmt.Println("Bill saved")
+
+	default:
+		fmt.Println("Your choice is invalid")
+		promptOptions(i)
+	}
 }
 
 func main() {
