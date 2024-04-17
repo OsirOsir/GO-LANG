314f507a689b65432e3140e399be0a5d78904abf
Philip Osir <philiphenril@gmail.com>
2024-04-17 12:03:37 +0300

    updates

--- INTERFACE/vowel.go
+++ INTERFACE/vowel.go
@@ -12,7 +12,7 @@ func (ms myVowel) findVowel() []rune {
 	vowel := []rune{}
 
 	for _, r := range ms {
-		if r == 'a' && r == 'e' && r == 'i' && r == 'o' && r == 'u' {
+		if r == 'a' || r == 'e' || r == 'i' || r == 'o' || r == 'u' {
 			vowel = append(vowel, r)
 		}
 	}
@@ -21,6 +21,10 @@ func (ms myVowel) findVowel() []rune {
 
 func main() {
 	name := myVowel("Philip Osir Otieno")
+
+	// var vl VowelFinder
+
+	// vl = name
 
 	fmt.Printf("The vowels are %c \n", name.findVowel())
 }
