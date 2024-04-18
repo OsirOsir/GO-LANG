7e62c768ea426a86221272578192803b4ce62d4e
Philip Osir <philiphenril@gmail.com>
2024-04-18 12:59:45 +0300

    Auto commit Thu, Apr 18, 2024 12:59:44 PM

---
+++ INTERFACE/CalculateSalary.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+type SalaryCalcultar interface {
+	calculateSalary() float64
+}
+
+type Pamanent struct {
+	empID     string
+	bSalary   float64
+	allwance  float64
+	dedaction float64
+}
+
+type Contracted struct {
+	empID   string
+	bSalary float64
+}
+
+func (p Pamanent) calculateSalary() float64 {
+	return (p.bSalary + p.allwance) - p.dedaction
+}
+
+func (c Contracted) calculateSalary() float64 {
+	return c.bSalary
+}
+
+func totalExpence(s []SalaryCalcultar) {
+	te := 0.0
+	for _, value := range s {
+		te += value.calculateSalary()
+	}
+
+	fmt.Printf("Total Employee Expences is USD %.2f\n", te)
+}
+
+func main() {
+	emp1 := Pamanent{
+		empID:     "Philip Osir",
+		bSalary:   36999.33,
+		allwance:  8000.9,
+		dedaction: 400.88,
+	}
+
+	emp2 := Pamanent{
+		empID:     "Kyle Hope",
+		bSalary:   26800.3,
+		allwance:  5000.9,
+		dedaction: 400.88,
+	}
+
+	emp3 := Contracted{
+		empID:   "Iddah Awuor",
+		bSalary: 56777.2,
+	}
+
+	employee := []SalaryCalcultar{emp1, emp2, emp3}
+
+	totalExpence(employee)
+}
