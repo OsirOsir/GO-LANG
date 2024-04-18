a88e5470c6c8553c611b49c521d9211055cc7ca7
Philip Osir <philiphenril@gmail.com>
2024-04-18 07:31:46 +0300

    Auto commit Thu, Apr 18, 2024  7:31:46 AM

--- INTERFACE/typeSwitch.go
+++ INTERFACE/typeSwitch.go
@@ -2,16 +2,14 @@ package main
 
 import "fmt"
 
-var i interface
-
 func like(i interface{}) {
 	switch i.(type) {
 	case string:
 		fmt.Printf("I learn at %s\n", i.(string))
 	case int:
-		fmt.Printf("And I am %d years old \n ", i.(int))
+		fmt.Printf("And I am %d years old\n ", i.(int))
 	default:
-		fmt.Printf("Unknown type")
+		fmt.Printf("Unknown type\n")
 	}
 }
 
