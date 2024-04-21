ac72407b0e1bbd421af2a17bd99be753b5af2d68
Philip Osir <philiphenril@gmail.com>
2024-04-21 07:15:29 +0300

    Auto commit Sun, Apr 21, 2024  7:15:29 AM

--- CONSTANT/notAllowed.go
+++ CONSTANT/notAllowed.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"math"
 )
 
 func main() {
-	a := math.Sqrt(4)
+	var a = math.Sqrt(4)
 	const b = math.Sqrt(4)
-	fmt.Printf("Squre root of %.1f is %.1f\n", a)
+	// fmt.Printf("Squre root of %.1f is %.1f\n", a)
 }
---
+++ CONSTANT/stringConst.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	const n = "Sam"
+	var name = n
+
+	fmt.Printf("type %T value %v\n", name, name)
+}
