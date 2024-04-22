567b05b109d640320837490bd28cb5a4e2813608
Philip Osir <philiphenril@gmail.com>
2024-04-23 00:35:37 +0300

    Auto commit Tue, Apr 23, 2024 12:35:35 AM

--- STRINGS/Creatingstringslicrune.go
+++ STRINGS/Creatingstringslicrune.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	runes := []rune{0x0053, 0x0065, 0x00f1, 0x006f, 0x0072}
+	// runes := []rune{0x0053, 0x0065, 0x00f1, 0x006f, 0x0072}
 
-	str := string(runes)
-	fmt.Println(str)
+	// str := string(runes)
+	// fmt.Println(str)
+
+	name := "philipojkhAIKJj"
+	runeSlice := []rune(name)
+
+	fmt.Printf("%c %d\n", runeSlice, utf8.RuneCountInString(name))
 }
