9b21b3b4ba947598aab6e9480b8484f708e7764b
Philip Osir <philiphenril@gmail.com>
2024-04-22 08:31:51 +0300

    Auto commit Mon, Apr 22, 2024  8:31:49 AM

---
+++ STRINGS/UTFENCODDED.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := "Hello, 世界! 😀"
+
+	// Print the string
+	fmt.Println(str)
+
+	// Print the length of the string
+	fmt.Println("Length:", len(str)) //
+}
