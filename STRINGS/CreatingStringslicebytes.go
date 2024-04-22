673731b622ccb9c20b53cfb98fc226c9efe0d9c9
Philip Osir <philiphenril@gmail.com>
2024-04-23 00:01:00 +0300

    Auto commit Tue, Apr 23, 2024 12:01:00 AM

--- STRINGS/CreatingStringslicebytes.go
+++ STRINGS/CreatingStringslicebytes.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	byteSlice := []byte{0x43, 0x61, 0x66, 0xC3, 0xA9}
 
-	for _, i := range byteSlice {
-		fmt.Printf("%x ", i)
-	}
-	fmt.Println()
+	// for _, i := range byteSlice {
+	// 	fmt.Printf("%x ", i)
+	// }
+	// fmt.Println()
 
-	// str := string(byteSlice)
-	// fmt.Println(str)
+	str := string(byteSlice)
+	fmt.Println(str)
 }
